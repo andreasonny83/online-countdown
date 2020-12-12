6dd13f31b9e37d0d8279a9cbe9375cc02d6a120e
andreasonny83 <andreasonny83@gmail.com>
2020-12-12 15:14:00 +0000

    Update create function

--- packages/server/operations/create.js
+++ packages/server/operations/create.js
@@ -2,11 +2,33 @@
 const uuid = require('uuid');
 const dynamodb = require('./dynamodb');
 
+// function changeTimezone(date, ianatz) {
+//   // suppose the date is 12:00 UTC
+//   const invDate = new Date(
+//     date.toLocaleString('en-US', {
+//       timeZone: ianatz,
+//     })
+//   );
+
+//   // then invdate will be 07:00 in Toronto
+//   // and the diff is 5 hours
+//   const diff = date.getTime() - invDate.getTime();
+
+//   // so 12:00 in Toronto is 17:00 UTC
+//   return new Date(date.getTime() - diff); // needs to substract
+// }
+
+// E.g.
+// var here = new Date();
+// var there = changeTimezone(here, 'America/Toronto');
+
+// console.log(`Here: ${here.toString()}\nToronto: ${there.toString()}`);
+
 module.exports.create = (event, context, callback) => {
   const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
 
-  if (!data || typeof data.text !== 'string') {
+  if (!data || typeof data !== 'object') {
     console.error('Validation Failed');
     callback(null, {
       statusCode: 400,
@@ -20,9 +42,16 @@ module.exports.create = (event, context, callback) => {
     TableName: process.env.DYNAMODB_TABLE,
     Item: {
       id: uuid.v1(),
-      target: data.target,
       createdAt: timestamp,
       updatedAt: timestamp,
+      name: data.name,
+      day: data.day,
+      month: data.month,
+      year: data.year,
+      hour: data.hour,
+      min: data.min,
+      sec: data.sec,
+      locale: data.locale,
     },
   };
 
